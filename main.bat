bac79edb5c4af6fdad1b9d7b0dbcff12bceaa54c
ErtugrulSener <ertugrulsener@hotmail.de>
2022-06-26 08:32:12 +0200

    Added checks to pre-install batch script to check if git installed and pull repository if not exists.

--- main.bat
+++ main.bat
@@ -1,25 +1,73 @@
 @echo off
 
+@clear
+
+:: Variables
+set GIT_REMOTE_URL=https://github.com/ErtugrulSener/Automation.git
+
 :: cd to the directory the script is in
 @cd %~dp0
+
+
 
 :: Check for python 3 installation on system
 python --version 3 > nul
 
-if errorlevel == 1 (
+if %ERRORLEVEL% NEQ 0 (
 	goto installPythonViaChocolatey
 ) else (
-	goto executeMainScript
+	goto skipPythonInstallation
 )
 
 :installPythonViaChocolatey:
 	echo Installing chocolatey windows package manager
 	@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 	
-	echo Installing Python3
+	echo Installing Python3 since it's missing
 	@choco install -y python3
 
-	goto executeMainScript
+:skipPythonInstallation
 
-:executeMainScript
-	python install.py %*
+
+
+:: Check for git installation on system
+git --version > nul
+
+if %ERRORLEVEL% NEQ 0 (
+	goto installGit
+) else (
+	goto skipGitInstallation
+)
+
+:installGit:
+	echo Installing Git since it's missing
+	@choco install -y git
+
+:skipGitInstallation
+
+
+
+:: Check if git repo is checked out here already
+if not exist Automation\ (
+	@SET remote_url_in_folder=hi
+	FOR /F %%I IN ('git config --get remote.origin.url') DO @SET "remote_url_in_folder=%%I"
+	
+	if "%remote_url_in_folder%"=="%GIT_REMOTE_URL%" (
+		goto skipFetchingGitRepository
+	) else (
+		goto fetchGitRepository
+	)
+) else (
+	@cd Automation
+	goto skipFetchingGitRepository
+)
+
+:fetchGitRepository:
+	echo Fetching git repository since it's missing
+	@git clone --quiet %GIT_REMOTE_URL% Automation > nul
+	@cd Automation
+
+:skipFetchingGitRepository
+
+
+python install.py %*
