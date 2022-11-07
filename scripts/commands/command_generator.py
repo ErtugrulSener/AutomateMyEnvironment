d324660393493cc6a974d3d3d7fa7be03ed0c6be
ErtugrulSener <ertugrulsener@hotmail.de>
2022-11-07 08:31:01 +0100

    Fixed CommandGenerator class putting magic methods into command like __len__ and not calling second attr (like parameters -> status -> parameters)

--- scripts/commands/command_generator.py
+++ scripts/commands/command_generator.py
@@ -3,11 +3,9 @@ from scripts.logging.logger import Logger
 logger = Logger.instance()
 
 
+# noinspection PyAttributeOutsideInit
 class CommandGenerator:
     def __init__(self):
-        self.command = []
-        self.last_argument = None
-
         self.reset()
 
     def __getattr__(self, item):
@@ -21,7 +19,12 @@ class CommandGenerator:
         return self
 
     def add_to_command(self, partial_command):
-        self.command.append(str(partial_command))
+        partial_command = str(partial_command)
+
+        if partial_command.startswith("__"):
+            return
+
+        self.command.append(partial_command)
 
     def __call__(self, *args, **kwargs):
         if not self.last_argument:
@@ -32,18 +35,21 @@ class CommandGenerator:
         elif self.last_argument != "parameters":
             self.add_to_command(self.last_argument)
 
-        if args and len(args) > 0:
-            for arg in args:
-                self.add_to_command(arg)
+        for arg in args:
+            self.add_to_command(arg)
+
+        self.__delattr__(self.last_argument)
+        self.last_argument = ""
 
         return self
 
     def reset(self):
         self.command = []
-        self.last_argument = None
+        self.last_argument = ""
         return self
 
     def get(self, log_command=True):
+        logger.debug(self.command)
         command = ' '.join(self.command)
 
         if log_command:
